168a87e78cd3bb0c936a8409818168afc6a396b3
Ifeoma <ojiniifeoma@gmail.com>
2021-06-22 13:39:31 -0700

    Modified 14 again

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include "holberton.h"
-
 /**
- *main - Starting point
- *printFibonacciNumbers - function that prints first 50 Fibonacci Numbers
- *Return 0
- *@n: int argument
+ * main - starting point
+ *
+ *Return: Always 0
  */
 
-void printFibonacciNumbers(void)
+int main(void)
 {
 	long int f1 = 1;
 
@@ -18,17 +16,15 @@ void printFibonacciNumbers(void)
 
 	long int next;
 
-	for (i = 1; i < 50; i++)
+	for (i = 1; i <= 50; i++)
 	{
-		printf("%ld, ", f2);
+		if (f2 == 1)
+			printf("%ld", f2);
+		else
+			printf(", %ld", f2);
 		next = f1 + f2;
 
 		f1 = f2;
 		f2 = next;
 	}
-}
-int main()
-{
-	printFibonacciNumbers();
-	return 0;
 }
