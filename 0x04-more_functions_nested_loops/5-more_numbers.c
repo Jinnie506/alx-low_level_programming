34368e6d6d1bbfe4b31f9fcb898199df3525ac69
Ifeoma <ojiniifeoma@gmail.com>
2021-06-24 05:07:46 -0700

    Modified task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,15 @@ void more_numbers(void)
 
 	int b;
 
-	for (a = 0; a < 10, a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
 			if (b > 9)
 			{
 				_putchar((b / 10) + '0');
-				_putchar((b % 10) + '0');
 			}
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
