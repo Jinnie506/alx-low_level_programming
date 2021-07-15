b9519c4c0c39130459369251f3ff906bd60e9bd7
Ifeoma <ojiniifeoma@gmail.com>
2021-07-15 10:57:10 -0700

    Fixed task 1 again

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,14 +18,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i = 0;
 	else
 	{
-		for (i = 0; s1[i]; i++)
+		for (i = 0; s1[i] != '\0'; i++)
 			;
 	}
 	if (s2 == NULL)
 		j = 0;
 	else
 	{
-		for (j = 0; s2[j]; j++)
+		for (j = 0; s2[j] != '\0'; j++)
 			;
 	}
 	if (j > n)
