7be208a28d8be488bc6435ac1ebcd45165d1eee3
Ifeoma <ojiniifeoma@gmail.com>
2021-09-22 03:23:47 -0700

    tried again

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _abs - function that gives the absolute value of a number
+ * @n:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
